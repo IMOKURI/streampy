bddc3d5f68ce9d7d931261b6987ac023bb760cdd
Yoshio Sugiyama <nenegi.01mo@gmail.com>
2022-05-28 15:35:27 +0900

    feat: Add dtype.

--- streampy/streampy.py
+++ streampy/streampy.py
@@ -16,36 +16,31 @@ class StreamPy:
         self,
         values: np.ndarray,  # 2-dim with any size and any dtype
         columns: List[str],
-        length: int = 0,
+        dtype: Type = None,
         default_value: Any = None,
+        length: int = 0,
     ):
         """
         Args:
             values (np.ndarray): Numpy array to store data.
-            length (int): Number of data currently stored.
+            columns (List[str]): Column name of store data.
+            dtype (Type): Data type of store data.
             default_value (int): Value to be inserted when an exception is raised.
+            length (int): Number of data currently stored.
         """
         self.values = values
-        self.dtype = values.dtype
-        self.length = length
+        self.columns = columns
+        self.dtype = dtype if dtype is not None else values.dtype
         self.default_value = default_value
+        self.length = length
 
         self.capacity = values.shape[0]
-        self.columns = columns
 
     def __getitem__(self, item):
         return self.values[item]
 
     def __len__(self):
         return self.length
-
-    @property
-    def shape(self):
-        return self.length, self.values.shape
-
-    @property
-    def to_df(self) -> pd.DataFrame:
-        return pd.DataFrame(self.values, columns=self.columns)
 
     @classmethod
     def empty(
@@ -58,6 +53,14 @@ class StreamPy:
         values = np.zeros((capacity, len(columns)), dtype=dtype)
 
         return cls(values, columns, default_value=default_value)
+
+    @property
+    def shape(self):
+        return self.values.shape
+
+    @property
+    def to_df(self) -> pd.DataFrame:
+        return pd.DataFrame(self.values, columns=self.columns, dtype=self.dtype)
 
     def extend(self, array: np.ndarray):
         n_row, n_col = array.shape
@@ -75,15 +78,6 @@ class StreamPy:
         self.values[self.length : self.length + n_row] = array
         self.length += n_row
 
-    def last_n(self, n: int) -> np.ndarray:
-        res = self.values[max(self.length - n, 0) : self.length]
-
-        if len(res) == n:
-            return res
-
-        res = np.concatenate([np.zeros((n - len(res), len(self.columns)), dtype=res.dtype), res])
-        return res
-
     def _grow(self, min_capacity):
         capacity = max(int(1.5 * self.capacity), min_capacity)
         new_data_len = capacity - self.capacity
@@ -96,3 +90,12 @@ class StreamPy:
             ]
         )
         self.capacity += new_data_len
+
+    def last_n(self, n: int) -> np.ndarray:
+        res = self.values[max(self.length - n, 0) : self.length]
+
+        if len(res) == n:
+            return res
+
+        res = np.concatenate([np.zeros((n - len(res), len(self.columns)), dtype=res.dtype), res])
+        return res
